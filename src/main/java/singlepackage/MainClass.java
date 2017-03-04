7eba472adea3268ec8e7b24df887c7419ce6cf18
eshwar_op <eshwar_op>
2017-03-04 20:08:49 +0000

    let us push

---
+++ src/main/java/singlepackage/MainClass.java
@@ -0,0 +1,15 @@
+package singlepackage;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Subbu how are you doing");
+		System.out.println("This is your first java projecct");
+		System.out.println("Lets see how it goes");
+		
+		
+		
+		
+		
+	}
+}
